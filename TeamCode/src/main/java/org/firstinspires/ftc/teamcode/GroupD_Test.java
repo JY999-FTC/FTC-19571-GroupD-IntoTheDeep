58420a71183cd541e674b6fec81c5353b3dc110b
JY999-FTC <jeromeyang999@gmail.com>
2025-03-29 18:18:44 -0400

    Update GroupD_Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GroupD_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GroupD_Test.java
@@ -10,6 +10,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
@@ -21,9 +22,14 @@ public class GroupD_Test extends LinearOpMode {
     double servoPosition = 0;
 
     // Declarations of hardware, Best practice to declare in class because if declare in runOpMode can only be used in there
-    DcMotorEx MotorT;
 
+    // This is declared with a encoder to track # of rotations. DcMotor for no encoder
+    DcMotorEx MotorT;
+    //DcMotor MotorT;
+
+    // This is declared as a servo, which moves to a position, it could also be CRServo which is a Continuous Servo. The Servo can be programmed between the 2 modes
     Servo ServoT;
+    //CRServo ServoT;
 
     @Override
 
@@ -31,8 +37,10 @@ public class GroupD_Test extends LinearOpMode {
 
         // Declarations of hardware, make sure name & configuration matches
         MotorT = hardwareMap.get(DcMotorEx.class, "MotorT");
+        //MotorT = hardwareMap.get(DcMotor.class, "MotorT"); //For no encoder motor
 
         ServoT = hardwareMap.get(Servo.class, "ServoT");
+        //ServoT = hardwareMap.get(CRServo.class, "ServoT"); //For Continuous Servo
 
 
         // Set up motor
@@ -50,24 +58,41 @@ public class GroupD_Test extends LinearOpMode {
         // Wait for start button
         waitForStart();
 
+        // If not ready then do not run code below, return to top and run code again
         if (isStopRequested()) return;
 
         
         // While loop runs multiple times per second
         while (opModeIsActive()) {
 
-            // move motor
-            if (gamepad1.right_stick_x != 0) {
+            // Move Motor
+            if (gamepad1.right_stick_x != 0) { // if statement, there is 2 gamepads and gamepad1.right_stick_x means a # between -1 to 1
                 motorPosition += gamepad1.right_stick_x;
-                MotorT.setTargetPosition((int) motorPosition);
+                MotorT.setTargetPosition((int) motorPosition);// Motor has a encoder so can move to a target position (int) to get rid of decimals
             }
 
-            // move servo
+            // Move no Encoder Motor
+            /*
+            if (gamepad1.right_stick_x != 0) { // if statement, there is 2 gamepads and gamepad1.right_stick_x means a # between -1 to 1
+                MotorT.setPower((gamepad1.right_stick_x);// Motor has a encoder so can move to a target position (int) to get rid of decimals
+            }
+            */
+
+            // Move Servo
             if (gamepad1.left_stick_x != 0) {
-                servoPosition += gamepad1.left_stick_x;
-                ServoT.setPosition(servoPosition);
+                servoPosition += gamepad1.left_stick_x * 0.1;
+                ServoT.setPosition(servoPosition);// position only for servo, CRServo is setDirection()
             }
 
+            // Move CRServo
+            /*
+            if (gamepad1.left_stick_x != 0) {
+                ServoT.setPower(gamepad1.left_stick_x);// position only for servo, CRServo is setDirection()
+            }
+            */
+
+
+            // This is a Method, they are defined and can be used multiple times to save time, energy and space
             updateTelemetry();
 
         }// while end
@@ -75,7 +100,7 @@ public class GroupD_Test extends LinearOpMode {
     }// OpMode end
 
 
-    // this is a method outside of the while loop, can be used inside the loop. Tlelmentry is the display on the right, can display text & images.
+    // This is a method outside of the while loop, can be used inside the loop. Telemetry is the display on the right.
     public void updateTelemetry() {
         telemetry.addData("Left JoyStick: ", gamepad1.left_stick_x);
         telemetry.addLine();

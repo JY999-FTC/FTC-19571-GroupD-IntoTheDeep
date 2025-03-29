c69b05700fccac7adbc92a00dc016edbf69d87b7
JY999-FTC <jeromeyang999@gmail.com>
2025-03-29 15:59:57 -0400

    Update GroupD_Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GroupD_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GroupD_Test.java
@@ -2,18 +2,18 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cCompassSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@TeleOp
 
 public class GroupD_Test extends LinearOpMode {
 
     DcMotorEx MotorT;
 
     Servo ServoT;
-
-
 
     public void runOpMode() throws InterruptedException {
 

818a2a9bd317e8fbe24f55a2b60d85be7b3de6e3
JY999-FTC <jeromeyang999@gmail.com>
2025-03-29 18:54:38 -0400

    Update GroupD_Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GroupD_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GroupD_Test.java
@@ -11,6 +11,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp
@@ -24,23 +25,23 @@ public class GroupD_Test extends LinearOpMode {
     // Declarations of hardware, Best practice to declare in class because if declare in runOpMode can only be used in there
 
     // This is declared with a encoder to track # of rotations. DcMotor for no encoder
-    DcMotorEx MotorT;
-    //DcMotor MotorT;
+    //DcMotorEx MotorT;
+    DcMotor MotorT;
 
     // This is declared as a servo, which moves to a position, it could also be CRServo which is a Continuous Servo. The Servo can be programmed between the 2 modes
-    Servo ServoT;
-    //CRServo ServoT;
+    //Servo ServoT;
+    CRServo ServoT;
 
     @Override
 
     public void runOpMode() throws InterruptedException {
 
         // Declarations of hardware, make sure name & configuration matches
-        MotorT = hardwareMap.get(DcMotorEx.class, "MotorT");
-        //MotorT = hardwareMap.get(DcMotor.class, "MotorT"); //For no encoder motor
+        //MotorT = hardwareMap.get(DcMotorEx.class, "MotorT");
+        MotorT = hardwareMap.get(DcMotor.class, "MotorT"); //For no encoder motor
 
-        ServoT = hardwareMap.get(Servo.class, "ServoT");
-        //ServoT = hardwareMap.get(CRServo.class, "ServoT"); //For Continuous Servo
+        //ServoT = hardwareMap.get(Servo.class, "ServoT");
+        ServoT = hardwareMap.get(CRServo.class, "ServoT"); //For Continuous Servo
 
 
         // Set up motor
@@ -49,10 +50,10 @@ public class GroupD_Test extends LinearOpMode {
         MotorT.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE); // if no electricity then use power to stop motor. Good for linear slidwe
         MotorT.setTargetPosition(0); // Target position of robo is 0
         MotorT.setMode(DcMotor.RunMode.RUN_TO_POSITION); // Spin to the target position
-        MotorT.setVelocity(2000); // Uses Encoder to detect and adjust velocity to constant a RPM
+        //MotorT.setVelocity(2000); // Uses Encoder to detect and adjust velocity to constant a RPM
 
         // Set up Servo
-        ServoT.setPosition(0); // Reset the servo, if CRServo then will spin
+        //ServoT.setPosition(0); // Reset the servo, if CRServo then will spin
 
         // Wait for start button
         waitForStart();
@@ -65,30 +66,36 @@ public class GroupD_Test extends LinearOpMode {
         while (opModeIsActive()) {
 
             // Move Motor
+            /*
             if (gamepad1.right_stick_x != 0) { // if statement, there is 2 gamepads and gamepad1.right_stick_x means a # between -1 to 1
-                motorPosition += gamepad1.right_stick_x;
-                MotorT.setTargetPosition((int) motorPosition);// Motor has a encoder so can move to a target position (int) to get rid of decimals
+                motorPosition += gamepad1.right_stick_x * 5; // motorPosition is a varible and joystick adjust it
+                MotorT.setTargetPosition((int) motorPosition); // Motor has a encoder so can move to a target position, (int) to get rid of decimals
             }
+             */
 
             // Move no Encoder Motor
-            /*
-            if (gamepad1.right_stick_x != 0) { // if statement, there is 2 gamepads and gamepad1.right_stick_x means a # between -1 to 1
-                MotorT.setPower((gamepad1.right_stick_x);// Motor has a encoder so can move to a target position (int) to get rid of decimals
+
+            if (gamepad1.right_stick_x != 0) {
+                MotorT.setDirection(DcMotorSimple.Direction.FORWARD); // set direction of movement
+                MotorT.setTargetPosition(100);
+                MotorT.setPower(gamepad1.right_stick_x); // Motor power is set by joystick
             }
-            */
+
 
             // Move Servo
-            if (gamepad1.left_stick_x != 0) {
-                servoPosition += gamepad1.left_stick_x * 0.002;
-                ServoT.setPosition(servoPosition);// position only for servo, CRServo is setDirection()
-            }
-
-            // Move CRServo
             /*
             if (gamepad1.left_stick_x != 0) {
-                ServoT.setPower(gamepad1.left_stick_x);// position only for servo, CRServo is setDirection()
+                servoPosition += gamepad1.left_stick_x * 0.002; // servoPosition is a varible adjusted by joystick. Note Servo can only go 0 to 1 but varible can go higher
+                ServoT.setPosition(servoPosition); // position only for servo, CRServo is setDirection()
             }
             */
+
+            // Move CRServo
+
+            if (gamepad1.left_stick_x != 0) {
+                ServoT.setPower(gamepad1.left_stick_x); // position only for servo, CRServo is setDirection()
+            }
+
 
 
             // This is a Method, they are defined and can be used multiple times to save time, energy and space
@@ -105,13 +112,13 @@ public class GroupD_Test extends LinearOpMode {
         telemetry.addLine();
         telemetry.addData("Right JoyStick: ", gamepad1.right_stick_x);
         telemetry.addLine();
-        telemetry.addData("Motor Target Position(Only w/encoder):", motorPosition);
+        telemetry.addData("Motor Target Position(Need Encoder):", motorPosition);
         telemetry.addLine();
-        telemetry.addData("Servo Target Position(Only Servo Mode): ", servoPosition);
+        telemetry.addData("Servo Target Position(Need Servo Mode): ", servoPosition);
         telemetry.addLine();
         telemetry.addData("Motor Actual Position:", MotorT.getCurrentPosition());
         telemetry.addLine();
-        telemetry.addData("Servo Actual Position: ", ServoT.getPosition());
+        telemetry.addData("Servo Actual Position: ", ServoT.getDirection());
         telemetry.addLine();
         telemetry.update();
     }//update telemetry end
